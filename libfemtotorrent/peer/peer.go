01ae9fa3967679ceffe295c9bb7b0b5962cfdd3d
Chandler Swift <chandler@chandlerswift.com>
2022-03-06 20:09:48 -0600

    Add basic connection management

--- libfemtotorrent/peer/peer.go
+++ libfemtotorrent/peer/peer.go
@@ -2,6 +2,7 @@ package peer
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
@@ -10,12 +11,13 @@ import (
 )
 
 type Peer struct {
-	IPAddress  net.IP
-	Port       uint16
-	Choked     bool
-	Interested bool
-	conn       net.Conn
-	ID         []byte
+	IPAddress      net.IP
+	Port           uint16
+	OutgoingChoked bool
+	IncomingChoked bool
+	Interested     bool // TODO: for both sides of the connection
+	conn           net.Conn
+	ID             []byte
 }
 
 func (p *Peer) Handle(tf torrentfile.TorrentFile) (err error) {
@@ -23,28 +25,51 @@ func (p *Peer) Handle(tf torrentfile.TorrentFile) (err error) {
 	if err != nil {
 		return
 	}
-	p.DeclareInterested()
 	for {
-		p.DeclareInterested()
+		if !p.IncomingChoked && !p.Interested {
+			log.Println("Declaring our interest")
+			log.Println("Requesting chunk 0@0")
+			p.Request(0, 0, 2<<14)
+			p.DeclareInterested()
+		}
 		buf := make([]byte, 65536)
 		var n int
 		n, err = p.conn.Read(buf)
 		if err != nil {
 			return
 		}
-		log.Printf("Received %v bytes: %q", n, buf[:n])
+		switch buf[4] { // message type
+		case 0:
+			log.Println("Received an incoming choke message")
+			p.IncomingChoked = true
+		case 1:
+			log.Println("Received an incoming unchoke message")
+			p.IncomingChoked = false
+		case 2:
+			log.Println("Received an incoming interested message")
+		case 3:
+			log.Println("Received an incoming uninterested message")
+		case 4:
+			var piece uint64
+			piece, err = binary.ReadUvarint(bytes.NewBuffer(buf[5:9]))
+			if err != nil {
+				return
+			}
+			log.Printf("Received a have message for piece %v", piece)
+		default:
+			log.Printf("In loop, received %v bytes: %q", n, buf[:n])
+		}
 	}
 }
 
 func (p *Peer) connect(tf torrentfile.TorrentFile) (err error) {
 	// Connections start out choked and not interested.
-	p.Choked = true
+	p.IncomingChoked = true
+	p.OutgoingChoked = true
 	p.Interested = false
 
 	// TODO: can we also do UDP?
-	fullHost := net.JoinHostPort(p.IPAddress.String(), fmt.Sprint(p.Port))
-	log.Println(fullHost)
-	p.conn, err = net.Dial("tcp", fullHost)
+	p.conn, err = net.Dial("tcp", net.JoinHostPort(p.IPAddress.String(), fmt.Sprint(p.Port)))
 	if err != nil {
 		return
 	}
@@ -85,14 +110,14 @@ func (p *Peer) connect(tf torrentfile.TorrentFile) (err error) {
 	if err != nil {
 		return
 	}
-	log.Printf("Received %d bytes: %q", n, buf[:n])
 	if !bytes.Equal([]byte("\x13BitTorrent protocol"), buf[:20]) {
 		return fmt.Errorf("Protocol mismatch; expected \"\\x13BitTorrent protocol\", got %q", buf[:20])
 	}
 	if !bytes.Equal(tf.InfoHash[:], buf[28:48]) {
 		return fmt.Errorf("InfoHash mismatch; expected %q, got %q", tf.InfoHash, buf[28:48])
 	}
-	// TODO: peer ID
+	// TODO: validate against what we expect
+	p.ID = buf[48:68]
 
 	if n > 68 {
 		// we got more bytes than we know what to do with
@@ -103,34 +128,52 @@ func (p *Peer) connect(tf torrentfile.TorrentFile) (err error) {
 }
 
 func (p *Peer) Choke() {
-	p.conn.Write([]byte{0})
+	p.conn.Write([]byte{0, 0, 0, 1, 0})
 }
 
 func (p *Peer) Unchoke() {
-	p.conn.Write([]byte{1})
+	p.conn.Write([]byte{0, 0, 0, 1, 1})
 }
 
 func (p *Peer) DeclareInterested() {
-	p.conn.Write([]byte{2})
+	p.conn.Write([]byte{0, 0, 0, 1, 2})
+	p.Interested = true
 }
 
 func (p *Peer) DeclareNotInterested() {
-	p.conn.Write([]byte{3})
+	p.conn.Write([]byte{0, 0, 0, 1, 3})
 }
 
 func (p *Peer) Have() {
-	p.conn.Write([]byte{4})
 	panic("unimplemented") // TODO
+	p.conn.Write([]byte{4})
 }
 
 func (p *Peer) Bitfield() {
-	p.conn.Write([]byte{5})
 	panic("unimplemented") // TODO
+	p.conn.Write([]byte{5})
 }
 
-func (p *Peer) Request() {
-	p.conn.Write([]byte{6})
-	panic("unimplemented") // TODO
+// 'request' messages contain an index, begin, and length. The last two are byte
+// offsets. Length is generally a power of two unless it gets truncated by the
+// end of the file. All current implementations use 2^14 (16 kiB), and close
+// connections which request an amount greater than that.
+func (p *Peer) Request(index, begin, length uint32) error {
+	msg := struct {
+		LengthPrefix uint32
+		MessageType  uint8
+		Index        uint32
+		Begin        uint32
+		Length       uint32
+	}{
+		LengthPrefix: 4 + 1 + 4 + 4 + 4,
+		MessageType:  6,
+		Index:        index,
+		Begin:        begin,
+		Length:       length,
+	}
+
+	return binary.Write(p.conn, binary.BigEndian, msg)
 }
 
 func (p *Peer) Piece() {
